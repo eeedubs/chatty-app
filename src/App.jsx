2fd078f0aec8db1a51eefb459f4dcdaba1f76de3
Eric Wilson <ericswilson94@gmail.com>
2018-11-29 02:21:44 +0000

    Updated branch to have messages be received by chatty_server

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,11 @@
 // Load up the application styles
 require('../styles/application.scss');
 
+
+// Web socket connected to chatty_server
+const socket = new WebSocket(`ws://${window.location.hostname}:3001`);
+
+
 // Render the top-level React component
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
@@ -15,9 +20,6 @@ import ReactDOM from 'react-dom';
 import ChatBar from './ChatBar.jsx'; 
 import Message from './Message.jsx';
 import MessageList from './MessageList.jsx';
-
-// const webSocket = require('ws');
-const wss = new WebSocket(`ws://${window.location.hostname}:3001`);
 
 function getMessage() {
   return new Promise((resolve) => {
@@ -32,64 +34,48 @@ class App extends Component {
     super(props);
     this.state = {
         currentUser: {name: 'Bob'},
-        messages : [
-          // {
-          //   type: 'incomingMessage',
-          //   content: 'I won\'t be impressed with technology until I can download food.',
-          //   username: 'Anonymous1'
-          // },
-          // {
-          //   type: 'incomingNotification',
-          //   content: 'Anonymous1 changed their name to nomnom',
-          // },
-          // {
-          //   type: 'incomingMessage',
-          //   content: 'I wouldn\'t want to download Kraft Dinner. I\'d be scared of cheese packet loss.',
-          //   username: 'Anonymous2'
-          // },
-          // {
-          //   type: 'incomingMessage',
-          //   content: '...',
-          //   username: 'nomnom'
-          // },
-          // {
-          //   type: 'incomingMessage',
-          //   content: 'I\'d love to download a fried egg, but I\'m afraid encryption would scramble it',
-          //   username: 'Anonymous2'
-          // },
-          // {
-          //   type: 'incomingMessage',
-          //   content: 'This isn\'t funny. You\'re not funny',
-          //   username: 'nomnom'
-          // },
-          // {
-          //   type: 'incomingNotification',
-          //   content: 'Anonymous2 changed their name to NotFunny',
-          // }
-        ]
+        messages: [],
+        socket: ''
       };
       this.addMessage = this.addMessage.bind(this);
   }
 
   componentDidMount() {
-    window.scrollTo({ bottom: 0, behavior: 'smooth' });
-    
-    wss.addEventListener('open', function () {
+    console.log('componentDidMount </App />');
+    const clients = [];
+    // if the socket is open, notify the user in the browser console and push the websocket
+    // variable into the client array
+    socket.addEventListener('open', function () {
       console.log('Woohoo it\'s working');
+      clients.push(socket);
+    });
+
+    // if a message is sent, log the message in the browser console
+    socket.addEventListener('message', function (msg) {
+      console.log('App.jsx: ', msg);
     })
+    // window.scrollTo({ bottom: 0, behavior: 'smooth' });
   }
   
+
   addMessage(message, name) {
+    // the message ID is equal to the length of the message array held in state plus 1
     let newId = this.state.messages.length + 1;
+    // create the messageItem object
     let newMessageItem = {
       id: newId,
       type: 'incomingMessage',
       content: message,
       username: name
     }
+    // add the next message to the end of the message array
     const newMessages = this.state.messages.concat(newMessageItem);
+    // get the message (0.5 second delay), then...
     getMessage().then(() => {
+      // replace the message object in state with the new message object
       this.setState({ messages: newMessages });
+      // send the stringified message to the websocket server
+      socket.send(JSON.stringify(newMessageItem));
     });
   }
 
